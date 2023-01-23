71e15eba7926541f79d1e6531afed6c117c534e8
ale2trelew <ale2trelew@gmail.com>
2023-01-23 18:28:53 -0300

    actualizacion de modelo de order y post

--- src/Routes/orderRoutes.js
+++ src/Routes/orderRoutes.js
@@ -1,43 +1,127 @@
 const express = require('express');
+const mercadopago = require('mercadopago');
 const Order = require('../Models/orderModel.js');
+const userSchema = require('../Models/modelUser.js');
 
 const orderRouter = express.Router();
 
 //CREATE ORDER
-orderRouter.post('/', async (req, res) => {
-    const {
-        usuario,
-        orderItems,
-        direccionEntrega,
-        metodoDePago,
-        precioEnvio,
-    } = req.body;
+orderRouter.post("/", async (req, res) => {
+    try {
+      const { items, user, total } = req.body;
 
-    if (orderItems && orderItems.length === 0) {
+      const respuesta = items.map((p) => {
+        return { ...p, stock: p.inventario - p.qty };
+      });
+
+      const arts = respuesta.map((prods) => {
+        return {
+          id: prods._id,
+          picture_url: prods.imagenes[0],
+          category_id: "fashion",
+          title: prods.modelo,
+          unit_price: prods.precio,
+          description: prods.descripcion,
+          quantity: prods.qty,
+          currency_id: "ARS",
+        };
+      });
+
+      const artOrder = respuesta.map((p) => {
+        return {
+          marca: p.marca,
+          modelo: p.modelo,
+          cantidad: p.qty,
+          precio: p.precio,
+          producto: p._id,
+        };
+      });
+
+      for (const q in respuesta) {
+        if (q.stock < 0) {
+          res.status(400).json({
+            msg: "sin inventario",
+          });
+        }
+      }
+
+      if (items && items.length === 0) {
         res.status(400).send("No hay productos en la orden de compra");
-    }
+      } else {
+        const preference = {
+          items: arts,
+          back_urls: {
+            success: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            failure: "",
+            pending: "",
+          },
+          auto_return: "approved",
+          binary_mode: true,
+        };
 
-    else {
+        const resp = await mercadopago.preferences.create(preference);
+        const userInfo = userSchema.findById(user);
+        const order = {
+          usuario: user,
+          direccionEntrega: {
+            direccion: userInfo.direccion,
+            ciudad: userInfo.ciudad,
+            pais: userInfo.pais,
+          },
+          metodoDePago: "mercadopago",
+          preferenceId: resp.body.id,
+          precioTotal: total,
+          estadoPago: "pending",
+          fechaCreacion: new Date()
+            .toLocaleString()
+            .replace(",", "")
+            .replace(/:.. /, " "),
+          estadoEntrega: false,
+        };
 
-        let totalprice = 0
-        orderItems.forEach(e => {
-            e.precio = e.cantidad * e.precio
-            totalprice = totalprice + e.precio
-        });
+        const nuevaOrden = new Order(order);
+        const saveOrder = await nuevaOrden.save();
 
-        const order = new Order({
-            usuario,
-            orderItems,
-            direccionEntrega,
-            metodoDePago,
-            precioEnvio,
-            precioTotal: totalprice
-        });
-
-        const createOrder = await order.save();
-        res.status(201).json(createOrder);
+        res.status(201).json({ msg: "Orden creada con exito", order });
+      }
+    } catch {
+        res.status(404);
     }
 });
+// orderRouter.post('/', async (req, res) => {
+//     const {
+//         usuario,
+//         orderItems,
+//         direccionEntrega,
+//         metodoDePago,
+//         precioEnvio,
+//     } = req.body;
+
+//     if (orderItems && orderItems.length === 0) {
+//         res.status(400).send("No hay productos en la orden de compra");
+//     }
+
+//     else {
+
+//         let totalprice = 0
+//         orderItems.forEach(e => {
+//             e.precio = e.cantidad * e.precio
+//             totalprice = totalprice + e.precio
+//         });
+
+//         const order = new Order({
+//             usuario,
+//             orderItems,
+//             direccionEntrega,
+//             metodoDePago,
+//             precioEnvio,
+//             precioTotal: totalprice
+//         });
+
+//         const createOrder = await order.save();
+//         res.status(201).json(createOrder);
+//     }
+// });
 
 // GET ORDER BY ID
 orderRouter.get('/:id', async (req, res) => {
